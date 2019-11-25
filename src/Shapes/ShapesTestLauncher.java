24aa39fbf7bce9ad73ad33c37969670b7317d295
FS <florin.schwappach@ur.de>
2019-11-25 10:24:32 +0100

    added ShapesTest-Starter and added new comments to ShapesTest.

---
+++ src/Shapes/ShapesTestLauncher.java
@@ -0,0 +1,11 @@
+package Shapes;
+
+import de.ur.mi.oop.launcher.GraphicsAppLauncher;
+
+
+public class ShapesTestLauncher {
+
+    public static void main(String[] args) {
+        GraphicsAppLauncher.launch("ShapesTest");
+    }
+}
