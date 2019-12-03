39a2a7f19801ca745ff63f5526a18b4ceaacf79f
kappa4head <lukas.jack@web.de>
2019-12-03 15:34:39 +0100

    Made Program work without ConsoleProgram
    The program now uses the BufferedReader class to read User Input in the IDE's built in console and no longer uses ConsoleProgram as a super class. Input mismatches are caught using try-catch. Should the user enter a wrong input type, eg. "zweitausend" for 'year', a error message is displayed and the method is restarted, so that the user can provide a new input.

--- src/library/LibraryApp.java
+++ src/library/LibraryApp.java
@@ -1,76 +1,111 @@
 package library;
 
-import acm.program.ConsoleProgram;
 
-import java.time.Year;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
  * Created by Alexander Bazo on 16/11/15.
+ * Modified by Lukas Jackermeier on 03/12/19
  */
-public class LibraryApp extends ConsoleProgram {
+public class LibraryApp {
     private static final int MAX_NUMBER_OF_MEDIA_IN_LIBRARY = 3;
-    private Media[] library;
+    private static Media[] library;
+    private static BufferedReader reader;
 
-    @Override
-    public void run() {
+
+    public static void main(String[] args) throws IOException {
+        initBufferedReader();
         initLibrary();
         saveMedia();
         printMedia();
+        reader.close();
     }
 
-    private void initLibrary() {
+    private static void initBufferedReader() {
+        reader = new BufferedReader(new InputStreamReader(System.in));
+    }
+
+    private static void initLibrary() {
         library = new Media[MAX_NUMBER_OF_MEDIA_IN_LIBRARY];
     }
 
-    private void saveMedia() {
+    private static void saveMedia() throws IOException {
         for(int i = 0; i < library.length; i++) {
             readAndSaveSingleMedia(i);
         }
     }
 
-    private void printMedia() {
-        println(library.length + " items stored in library: ");
+    private static void printMedia() {
+        System.out.println(library.length + " items stored in library: ");
         for(int i = 0; i < library.length; i++) {
-            println(library[i]);
+            System.out.println(library[i]);
         }
     }
 
-    private void readAndSaveSingleMedia(int indexInLibrary) {
-        int mediaType = readInt("Insert media type (1 = Book, 2 = DVD): ", 1, 2);
-        if(mediaType == 1) {
-            Book book = readBookInformation();
-            library[indexInLibrary] = book;
-        } else if(mediaType == 2) {
-            DVD dvd = readDVDInformation();
-            library[indexInLibrary] = dvd;
+   
+    private static void readAndSaveSingleMedia(int indexInLibrary) throws IOException, NumberFormatException {
+        try {
+            System.out.println("Insert media type (1 = Book, 2 = DVD): ");
+            int mediaType = Integer.parseInt(reader.readLine());
+            if (mediaType == 1) {
+                Book book = readBookInformation();
+                library[indexInLibrary] = book;
+            } else if (mediaType == 2) {
+                DVD dvd = readDVDInformation();
+                library[indexInLibrary] = dvd;
+            } else {
+                System.out.println("Error, please enter correct media type.");
+                readAndSaveSingleMedia(indexInLibrary);
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("Error, please enter correct media type.");
+            readAndSaveSingleMedia(indexInLibrary);
         }
     }
 
-    private Book readBookInformation() {
-        int year = readYear();
-        String title = readTitle();
-        String author = readLine("Author: ");
-        int numberOfPages = readInt("Number of pages: ");
-        Book book = new Book(year, title, author, numberOfPages);
-        return book;
+    private static Book readBookInformation() throws IOException, NumberFormatException {
+        try {
+            int year = readYear();
+            String title = readTitle();
+            System.out.println("Author: ");
+            String author = reader.readLine();
+            System.out.println("Number of pages: ");
+            int numberOfPages = Integer.parseInt(reader.readLine());
+            Book book = new Book(year, title, author, numberOfPages);
+            return book;
+        } catch (NumberFormatException e) {
+            System.out.println("Unexpected Input. Please try again.");
+            return readBookInformation();
+        }
     }
 
-    private DVD readDVDInformation() {
-        int year = readYear();
-        String title = readTitle();
-        int runtimeInMinutes = readInt("Runtime (minutes): ");
-        boolean hasBonusMaterial = readBoolean("Bonus material included (true/false): ");
-        DVD dvd = new DVD(year, title, runtimeInMinutes, hasBonusMaterial);
-        return dvd;
+    private static DVD readDVDInformation() throws IOException, NumberFormatException {
+        try {
+            int year = readYear();
+            String title = readTitle();
+            System.out.println("Runtime (minutes): ");
+            int runtimeInMinutes = Integer.parseInt(reader.readLine());
+            System.out.println("Bonus material included (true/false): ");
+            boolean hasBonusMaterial = Boolean.parseBoolean(reader.readLine());
+            DVD dvd = new DVD(year, title, runtimeInMinutes, hasBonusMaterial);
+            return dvd;
+        } catch (NumberFormatException e) {
+            System.out.println("Unexpected Input. Please try again.");
+            return readDVDInformation();
+        }
     }
 
-    private int readYear() {
-        int year = readInt("Year: ");
+    private static int readYear() throws IOException {
+        System.out.println("Year: ");
+        int year = Integer.parseInt(reader.readLine());
         return year;
     }
 
-    private String readTitle() {
-        String title = readLine("Title: ");
+    private static String readTitle() throws IOException {
+        System.out.println("Title: ");
+        String title = reader.readLine();
         return title;
     }
 
